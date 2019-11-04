6f9c17cfdbcf36028926c97aa415d7ac33c2b8dc
Gustav Lindqvist <gustav.lindqvist@ju.se>
2019-11-04 13:15:59 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='almaapi',
-      version='1.0',
+      version='1.1',
       description='Python module for making requests to the Alma API',
       url='https://github.com/JonkopingUniversityLibrary/almaapi',
       author='Gustav Lindqvist',
